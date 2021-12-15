346fe61659fc6265bbfbcaf6ffcc60fc62a8068a
jfgomez <jfgomez21@gmail.com>
2021-12-15 16:10:20 -0500

    Fixed back and strafe left issue that occurs on platforms where char is unsigned by default

--- src/engine/doom_main/d_ticcmd.h
+++ src/engine/doom_main/d_ticcmd.h
@@ -35,8 +35,8 @@
 // Mainly movements/button commands per game tick,
 // plus a checksum for internal state consistency.
 typedef struct {
-    char    forwardmove;    // *2048 for move
-    char    sidemove;    // *2048 for move
+    signed char    forwardmove;    // *2048 for move
+    signed char    sidemove;    // *2048 for move
     short    angleturn;    // <<16 for angle delta
     short    pitch;
     byte    consistency;    // checks for net game
